d5bf6dd52f88bb17ec91f57fbb54b33119cd5a6a
Ivan Shirshikov <ivansh@stud.ntnu.no>
2023-01-29 20:05:07 +0100

    More sculpular validation: TODO validate by name that only letters

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -44,3 +44,18 @@ export default {
   },
 };
 </script>
+<style>
+input {
+  height: 20px;
+  padding: 12px 20px;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  background-color: #f8f8f8;
+  resize: none;
+}
+label {
+  margin: 30px;
+}
+
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,38 +1,45 @@
 <template>
-  <div>
-    <h1>Give a feedback</h1>
+  <h1>Give a feedback</h1>
+
+  <div class="template">
     <form @submit.prevent="sendForm">
       <fieldset>
         <legend>Choose one of two option</legend>
         <BaseSelect
-          :options="state.categories"
-          v-model="state.comment.category"
+          :options="categories"
+          v-model="comment.category"
           label="Select category"
         ></BaseSelect>
+        <p v-if="v$.comment.category.$error">
+          {{ v$.comment.category.$errors[0].$message }}
+        </p>
       </fieldset>
 
       <fieldset>
         <legend>Personal information to five you feedback</legend>
-        <BaseInput
-          v-model="state.comment.name"
-          label="Name"
-          type="text"
-          error="This input has an error"
-        ></BaseInput>
+        <BaseInput v-model="comment.name" label="Name" type="text">
+        </BaseInput>
+        <p v-if="v$.comment.name.$error">
+        {{ v$.comment.name.$errors[0].$message }}
+        </p>
+        <br>
+        <BaseInput v-model="comment.mail" label="E-mail" type="text">
+        </BaseInput>
 
-        <BaseInput
-          v-model="state.comment.mail"
-          label="E-mail"
-          type="text"
-        ></BaseInput>
+        <p v-if="v$.comment.mail.$error">
+          {{ v$.comment.mail.$errors[0].$message }}
+        </p>
       </fieldset>
       <fieldset>
         <legend>Describe your experience</legend>
         <BaseInput
-          v-model="state.comment.description"
+          v-model="comment.description"
           label="Description"
           type="text"
         ></BaseInput>
+        <p v-if="v$.comment.description.$error">
+          {{ v$.comment.description.$errors[0].$message }}
+        </p>
       </fieldset>
 
       <button class="button -fill-gradient" type="submit">Submit</button>
@@ -43,54 +50,53 @@
 <script>
 import BaseInput from "@/components/BaseInput.vue";
 import BaseSelect from "@/components/BaseSelect.vue";
-import { reactive, computed } from 'vue'
 import axios from "axios";
-import useValidate from '@vuelidate/core'
-import { required } from '@vuelidate/validators'
+import useValidate from "@vuelidate/core";
+import { required, email, minLength } from "@vuelidate/validators";
 
 export default {
   components: { BaseSelect, BaseInput },
-  setup () {
-    const state = reactive({
+  setup() {
+    return { v$: useValidate() };
+  },
+  data() {
+    return {
       categories: ["experience", "bag"],
       comment: {
         category: "",
         name: "",
         mail: "",
         description: "",
-      }
       },
-    )
-    const rules = computed(() => {
-      return {
-        comment: {
-          category: { required },
-          name: { required },
-          mail: { required },
-          description: { required },
-        }
-      }
-    })
-    const v$ = useValidate(rules, state)
-    return { state, v$ }
+    };
+  },
+  validations() {
+    return {
+      comment: {
+        category: { required },
+        name: { required },
+        mail: { required, email },
+        description: { required, minLength: minLength(6) },
+      },
+    };
   },
   methods: {
     sendForm() {
-      this.v$.$validate() // checks all inputs
-      if(!this.v$.$error) {
+      this.v$.$validate(); // checks all inputs
+      if (!this.v$.$error) {
         axios
           .post(
             "https://my-json-server.typicode.com/Shershulia/CalculatorUsingVueJs/comments",
             this.comment,
             alert("Success")
           )
-          .then(function(response) {
+          .then(function (response) {
             console.log("Response", response);
           })
-          .catch(function(err) {
+          .catch(function (err) {
             console.log("Error", err);
           });
-      }else alert("Invalid")
+      } else alert("Invalid");
     },
   },
 };
@@ -105,5 +111,9 @@ legend {
   font-size: 28px;
   font-weight: 700;
   margin: 20px;
+}
+p {
+  font-size: 12px;
+  color: red;
 }
 </style>
